cb4710cf7d312953f394190a33545249ded5728c
llwslc <llwslc@gmail.com>
2020-07-23 23:53:36 +0800

    Update deploy.js

--- contract/deploy.js
+++ contract/deploy.js
@@ -2,3 +2,6 @@ const _TronWeb = require('tronweb');
 const { deployedPath, deploy, readJson, writeJson } = require('./util');
 const config = require('./config');
 const contract = config.contract;
+
+const deployedInfo = readJson(deployedPath);
+console.log('deployedInfo: ', deployedInfo);
