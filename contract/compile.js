a426ebff6ca878e2202e9367432b0f1289964efa
llwslc <llwslc@gmail.com>
2020-08-31 03:46:20 +0800

    Update compile.js

--- contract/compile.js
+++ contract/compile.js
@@ -41,4 +41,5 @@ const downloadFile = (uri, dest) => {
 
 const compile = async () => {
   const solUri = `https://tron-us.github.io/tron-solc-bin/bin/soljson_v${config.version}.js`;
+  const solPath = `./soljson_v${config.version}.js`;
 };
