838c4f500bcd9ea9346cdd61bc67cc84c8e1196c
llwslc <llwslc@gmail.com>
2020-11-11 02:04:30 +0800

    Update compile.js

--- contract/compile.js
+++ contract/compile.js
@@ -113,5 +113,6 @@ const compile = async () => {
         const version = JSON.parse(output.contracts[filename][contract].metadata).compiler.version;
         const obj = { name: f, abi, bytecode, version };
         fs.writeFileSync(targetFilePath, JSON.stringify(obj), 'utf8');
+      }
 };
 
